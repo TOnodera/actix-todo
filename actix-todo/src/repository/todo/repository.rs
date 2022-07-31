90e85d421e967b8c40254f7870cde87b3d3952af
t.onodera <t.onodera.1.2.1.5@gmail.com>
2022-07-31 09:07:25 +0000

    登録はできそう

---
+++ actix-todo/src/repository/todo/repository.rs
@@ -0,0 +1,30 @@
+use core::panic;
+
+use diesel::PgConnection;
+
+use crate::error::types::Error;
+use crate::repository::diesel::connection as diesel_connection;
+use crate::utils;
+
+use super::model::{NewTodo, Todo};
+pub struct TodoRepository {
+    connection: PgConnection,
+}
+
+impl TodoRepository {
+    pub fn new() -> Self {
+        let result = match utils::EnvFile::database_url() {
+            Ok(url) => diesel_connection::get_connection(url),
+            Err(e) => panic!("{}", e),
+        };
+        let connection = match result {
+            Ok(connection) => connection,
+            Err(e) => panic!("{}", e),
+        };
+
+        TodoRepository { connection }
+    }
+    pub fn insert(&self, todo: NewTodo) -> Result<i32, Error> {
+        Todo::insert(todo)
+    }
+}
