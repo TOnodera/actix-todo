0b0a94040865b44df3b75d221346db5157e2391f
t.onodera <t.onodera.1.2.1.5@gmail.com>
2022-08-12 00:45:10 +0000

    リポジトリの書き方変更

--- actix-todo/src/repository/todo/repository.rs
+++ actix-todo/src/repository/todo/repository.rs
@@ -1,12 +1,8 @@
-use core::panic;
-
-use diesel::r2d2::ConnectionManager;
-use diesel::PgConnection;
-use r2d2::{ManageConnection, PooledConnection};
-
 use crate::error::types::Error;
-use crate::repository::diesel::connection::{self as diesel_connection, Pool};
-use crate::utils;
+use crate::repository::diesel::schema::todos;
+use diesel::r2d2::ConnectionManager;
+use diesel::{prelude::*, PgConnection, QueryResult};
+use r2d2::PooledConnection;
 
 use crate::model::todo::{NewTodo, Todo};
 pub struct TodoRepository {
@@ -18,6 +14,14 @@ impl TodoRepository {
         TodoRepository { connection }
     }
     pub fn insert(&self, todo: NewTodo) -> Result<i32, Error> {
-        Todo::insert(&self.connection, todo)
+        let result = diesel::insert_into(todos::table)
+            .values(&todo)
+            .get_result::<Todo>(&self.connection) as QueryResult<Todo>;
+        match result {
+            Ok(row) => Ok(row.id),
+            Err(_) => Err(Error::DatabaseRuntimeError(
+                "データの登録に失敗しました。".to_string(),
+            )),
+        }
     }
 }
