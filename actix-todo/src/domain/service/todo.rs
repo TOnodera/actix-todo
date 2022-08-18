71a01be706d325c44651caaa3033c3e4de5c8b5e
t.onodera <t.onodera.1.2.1.5@gmail.com>
2022-08-18 13:00:08 +0000

    明日rud実装する。

--- actix-todo/src/domain/service/todo.rs
+++ actix-todo/src/domain/service/todo.rs
@@ -11,17 +11,13 @@ pub(crate) struct TodoDomainService;
 
 impl TodoDomainService {
     pub fn add_todo(repository: impl Crud, todo: NewTodoDomain) -> Result<i32, AppError> {
-        let result = TodoDomain::insert(
+        TodoDomain::insert(
             repository,
             RepositoryForCreate {
                 title: todo.title.clone(),
                 memo: todo.memo.clone(),
                 done: todo.done,
             },
-        );
-
-        result
-            .map(|id| id)
-            .map_err(|e| AppError::DatabaseRuntimeError(e.to_string()))
+        )
     }
 }
