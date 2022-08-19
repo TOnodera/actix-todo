3005f2bb00c4995b7e2a89fe4c633cfac55d3ca4
t.onodera <t.onodera.1.2.1.5@gmail.com>
2022-08-19 00:01:27 +0000

    RepositoryForCreate -> ModelForCreate

--- actix-todo/src/repository/model/todo.rs
+++ actix-todo/src/repository/model/todo.rs
@@ -5,7 +5,7 @@ use crate::repository::diesel::schema::todos;
 
 #[derive(Insertable)]
 #[table_name = "todos"]
-pub struct RepositoryForCreate {
+pub struct ModelForCreate {
     pub title: String,
     pub memo: Option<String>,
     pub done: bool,
